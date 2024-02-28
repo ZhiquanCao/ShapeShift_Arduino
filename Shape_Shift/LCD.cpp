709753d337dd72275098df6c1f6236c657863e08
Zhiquan Cao <zc1548@nyu.edu>
2024-02-28 12:08:40 -0500

    Fixed the shapeY

--- Shape_Shift/LCD.cpp
+++ Shape_Shift/LCD.cpp
@@ -15,21 +15,21 @@ int iterationCounter = 0;
 void display(bool side, bool shape)
 {
     int16_t centerX = tft.width() / 2;
-    int16_t centerY = tft.height() / 2 + 180;
+    int16_t centerY = tft.height() / 2;
 
     // Green background color for the entire screen
     tft.fillScreen(0x07E0); // Green background
 
     int shapeX, shapeY;
+    shapeY = (centerY - shapeSideLength) / 2 + 80;
+
     if (side == 0)
     { // Left side
         shapeX = (centerX - shapeSideLength) / 2;
-        shapeY = (centerY - shapeSideLength) / 2;
     }
     else
     { // Right side
         shapeX = centerX + (centerX - shapeSideLength) / 2;
-        shapeY = (centerY - shapeSideLength) / 2;
     }
 
     if (shape == 0)
@@ -71,7 +71,7 @@ void displayTextFlipped(char *text, uint16_t bgColor, uint8_t textSize)
     tft.setTextSize(textSize);
     tft.print(text);
 
-    tft.setRotation(0); // Restore the original rotation
+    // tft.setRotation(0); // Restore the original rotation
 }
 
 void displayText(char *text, uint16_t bgColor, uint8_t textSize)
@@ -79,11 +79,18 @@ void displayText(char *text, uint16_t bgColor, uint8_t textSize)
     int textWidth = strlen(text) * 6 * textSize; // Estimate width based on character count and size
     int textHeight = 8 * textSize;               // Assuming a standard font height of 8 pixels
     int screenWidth = tft.width();
+    Serial.println("width !!!!");
+    Serial.println(screenWidth);
+
     int screenHeight = tft.height();
+    Serial.println(screenHeight);
+
 
     // Calculate the center coordinates
     int x = (screenWidth - textWidth) / 2;
     int y = (screenHeight - textHeight) / 2;
+
+    tft.setRotation(1); // Rotate 90 degrees clockwise (adjust the value as needed)
 
     tft.fillRect(x, y, textWidth, textHeight, bgColor);
     tft.setCursor(x, y);
